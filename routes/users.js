385add9846d3eb48f382889719dd949dbf38358a
Danny <dongyunrhee@gmail.com>
2019-04-27 21:55:16 +0000

    Linted all the files.

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,16 @@
 "use strict";
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (knex) => {
-
   router.get("/", (req, res) => {
     knex
       .select("*")
       .from("users")
       .then((results) => {
         res.json(results);
-    });
+      });
   });
-
   return router;
 }
-
-
